bd924e6797d798a272494d614ee5663a5a5ae65a
Matthias Klan <matthias.klan@gmail.com>
2020-03-22 02:29:52 +0100

    Cleanup dependencies

--- src/form-request.ts
+++ src/form-request.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import qs from 'qs';
-import http from 'http';
+// import http from 'http';
 
 enum HttpMethods {
   GET = 'get',
@@ -15,50 +14,50 @@ export default function createFormRequestService(baseURL: string) {
     },
   });
 
-  const postNew = (path: string, payload?: Object) => {
-    const options = {
-      path,
-      hostname: baseURL,
-      port: 8000,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    };
+  // const postNew = (path: string, payload?: Object) => {
+  //   const options = {
+  //     path,
+  //     hostname: baseURL,
+  //     port: 8000,
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/x-www-form-urlencoded',
+  //     },
+  //   };
 
-    // @ts-ignore
-    // const data = new URLSearchParams(payload);
-    return new Promise((resolve, reject) => {
-      const req = http.request(options, res => {
-        console.log(`statusCode: ${res.statusCode}`);
-        let body = '';
-        res.on('data', d => {
-          body += d.toString();
-        });
+  //   // @ts-ignore
+  //   const data = new URLSearchParams(payload);
+  //   return new Promise((resolve, reject) => {
+  //     const req = http.request(options, res => {
+  //       console.log(`statusCode: ${res.statusCode}`);
+  //       let body = '';
+  //       res.on('data', d => {
+  //         body += d.toString();
+  //       });
 
-        res.on('end', () => {
-          console.log('bory', body);
-          resolve(body);
-        });
-        res.on('error', reject);
-      });
+  //       res.on('end', () => {
+  //         console.log('bory', body);
+  //         resolve(body);
+  //       });
+  //       res.on('error', reject);
+  //     });
 
-      req.write(qs.stringify(payload));
-      req.end();
-    });
-  };
+  //     req.write(data);
+  //     req.end();
+  //   });
+  // };
 
   const request = (method: HttpMethods) => async (path: string, payload?: Object) => {
-    console.log(method, path, qs.stringify(payload));
-    const params = qs.stringify(payload);
+    // @ts-ignore
+    const params = new URLSearchParams(payload);
+    if (process.env.PYLOADJS_DEBUG) console.log(method, path, params);
     // @ts-ignore
     const { data } = await transport[method](path, params);
     return data;
   };
 
   return {
-    get: request(HttpMethods.GET),
     post: request(HttpMethods.POST),
-    postNew,
+    // postNew,
   };
 }
